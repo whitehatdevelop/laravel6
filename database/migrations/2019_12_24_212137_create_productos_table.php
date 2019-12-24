d16730a15c0145930322644ef9b270d7043a085b
Alvaro <jorgealvarobarreravasquez@gmail.com>
2019-12-24 16:43:52 -0500

    Agregando modelo y migracion Producto

---
+++ database/migrations/2019_12_24_212137_create_productos_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nombre');
+            $table->string('slug');
+            $table->unsignedBigInteger('categoria_id');
+            $table->BigInteger('cantidad')->unsigned()->default(0);
+            $table->decimal('precio_actual',12,2)->unsigned()->default(0);
+            $table->decimal('precio_anterior',12,2)->unsigned()->default(0);
+            $table->integer('porcentaje_descuento')->unsigned()->default(0);
+            $table->text('descripcion_corta');
+            $table->text('descripcion_larga');
+            $table->text('especificaciones');
+            $table->text('datos_de_interes');
+            $table->unsignedBigInteger('visitas')->default(0);
+            $table->unsignedBigInteger('ventas')->default(0);
+            $table->string('estado');
+            $table->char('activo',2);
+            $table->char('sliderprincipal',2);
+            $table->timestamps();
+
+            $table->foreign('categoria_id')->references('id')->on('categorias');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
